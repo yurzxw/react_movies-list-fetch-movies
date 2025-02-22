a4a79d7108a048159431350f9e303a2298328c7f
Yura <yuradziavoruk5@gmail.com>
2025-02-23 01:32:52 +0200

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -44,7 +44,7 @@ export const FindMovie: React.FC<Props> = ({ movieList, setMovieList }) => {
               .then(foundedMovie => {
                 setMovie(foundedMovie);
               })
-              .finally(setLoading(false));
+              .finally(() => setLoading(false));
           }, 600);
         }}
       >
